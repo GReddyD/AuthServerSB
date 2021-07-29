16e026bd4302e85fe8f4b6c2ae9a7b84702ba35d
Aleksandr <GReddyd1990@gmail.com>
2021-07-29 22:54:50 +0300

    Add nginx settings in resources

--- src/main/java/com/netology/authserversb/controller/AuthorizationController.java
+++ src/main/java/com/netology/authserversb/controller/AuthorizationController.java
@@ -6,14 +6,12 @@ import com.netology.authserversb.exception.UnauthorizedUser;
 import com.netology.authserversb.service.AuthorizationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/")
 public class AuthorizationController {
 	AuthorizationService service;
 
